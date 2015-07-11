270f33eade7c278a035db55541f2813917546713
Jack Li <jack.lee2980@gmail.com>
2015-07-10 23:49:21 -0400

    Changed invite to use a meteor method

--- client/parties/controllers/partiesDetails.ng.js
+++ client/parties/controllers/partiesDetails.ng.js
@@ -1,5 +1,5 @@
 app.controller('PartyDetailsCtrl',
-	function ($scope, $stateParams, $meteor, $timeout) {
+	function ($scope, $stateParams, $meteor, $timeout, $rootScope) {
 		$scope.$meteorSubscribe('parties');
 		$scope.party = $meteor.object(Parties, $stateParams.partyId, false)
 		$scope.partyId = $stateParams.partyId;
@@ -14,13 +14,13 @@ app.controller('PartyDetailsCtrl',
 			$scope.party.description = "";
 		}
 		$scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
-		$scope.invite = function (user) {
-			$scope.party.invited = $scope.party.invited || {};
-			$scope.party.invited[user._id] = !$scope.party.invited[user._id];
-			$scope.party.save();
+		$scope.invite = function (user, add) {
+			if (typeof add === "undefined")
+				add = true;
+			$meteor.call('invite', $scope.party._id, user._id, add);
 		}
 		$scope.owner = getUser($scope.party.owner);
-		
+		$scope.isOwner = $scope.party.owner == $rootScope.currentUser._id;
 		$scope.$watch('party.invited', function () {
 			$scope.invitedUsers = Object.keys($scope.party.invited)
 				.filter(function (key) {
--- server/parties.js
+++ server/parties.js
@@ -25,7 +25,48 @@ Meteor.publish("parties", function (options, searchString) {
 			}
 		]
 	};
-	console.log(totalQuery);
 	Counts.publish(this, 'numberOfParties', Parties.find(totalQuery),{noReady:true});
 	return Parties.find(totalQuery, options);
 }) 
+
+Meteor.methods({
+	invite: function (partyId, userId, add) {
+		check(partyId, String);
+		check(userId, String);
+		var party = Parties.findOne(partyId);
+		if (!party)
+			throw new Meteor.Error(404, "No such party");
+		if (party.owner !== this.userId)
+			throw new Meteor.Error(404, "No such party");
+		if (party.public) {
+			throw new Meteor.Error(400, "This party is public. No need to invite people.")
+		}
+		if (userId !== party.owner) {
+			var setModifier = {};
+			setModifier['invited.' + userId] = !!add;
+			Parties.update(partyId, { $set: setModifier })
+			
+			if (add && Meteor.isServer) {
+				var from = contactEmail(Meteor.users.findOne(this.userId));
+				var to = contactEmail(Meteor.users.findOne(userId));
+				Email.send({
+					from: "noreply@socially.com",
+					to: to,
+					replyTo: from || undefined,
+					subject: "PARTY: " + party.name,
+					text:
+					"Hey, I just invited you to '" + party.name + "' on Socially." +
+					"\n\nCome check it out: " + Meteor.absoluteUrl() + "\n"
+				});
+			}
+		}	
+	}
+})
+
+var contactEmail = function (user) {
+    if (user.emails && user.emails.length)
+      return user.emails[0].address;
+    if (user.services && user.services.facebook && user.services.facebook.email)
+      return user.services.facebook.email;
+    return null;
+ };
